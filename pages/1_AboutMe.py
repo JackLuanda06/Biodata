eaf4acf3086bc6a38627d954d20f75f815c8c2ed
JackLuanda06 <dzaky.luanda2006@gmail.com>
2026-07-28 21:01:23 +0700

    draft 5

--- pages/1_AboutMe.py
+++ pages/1_AboutMe.py
@@ -13,7 +13,7 @@ with story:
     st.markdown("""
     Perjalanan profesional saya dimulai lebih awal sejak masa sekolah di SMK Raden Umar Said Kudus. Pada tahun 2023, saat masih duduk di kelas 11, saya mendapatkan kesempatan luar biasa untuk magang selama satu tahun penuh di RUS Animation Studio sebagai 3D Rigging Artist. 
     
-    Di sana, saya terlibat langsung dalam memproduksi berbagai proyek animasi besar seperti [Wakakibo Season 1](https://youtu.be/u_fSd-MHB0U?si=wDaZWDKzbpB-WPez), [La Luz de Aisha](https://youtu.be/dJtNhpyhflI?si=bGAi0X2qgUIWZqjP), [Nyla](https://youtu.be/vBnnBtp8JnI?si=EN-lu0lEdQIZ44SI), dan [Asa](https://youtu.be/CZltuDC_OPY?si=8JIhI-M0fsvUkCW7).
+    Di sana, saya terlibat langsung dalam memproduksi berbagai proyek animasi besar seperti [Wakakibo Season 1](https://youtu.be/u_fSd-MHB0U?si=wDaZWDKzbpB-WPez), [La Luz de Aisha](https://youtu.be/dJtNhpyhflI?si=bGAi0X2qgUIWZqjP), [Nyla](https://youtu.be/vBnnBtp8JnI?si=EN-lu0lEdQIZ44SI), [Asa](https://youtu.be/CZltuDC_OPY?si=8JIhI-M0fsvUkCW7), dan [Uang Ber$ih](https://youtu.be/33E4YSyUyQI?si=AIWPuJnHLf96PBF2).
     
     Setahun kemudian, pada tahun 2024, saya lulus dari SMK dengan nilai yang memuaskan dan membawa bekal skill yang sudah matang ditempa oleh industri. 
     
